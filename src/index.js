62de02b9cc47535db9172652149c4e7e154c316b
ProjmiX <balb310372@mail.ru>
2020-10-03 23:40:18 +0300

    feat: add solution for task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  var after = str;
+  do {
+      var befor = after;
+      for ([l, r] of bracketsConfig) {
+        after = after.replace(`${l}${r}`, '');
+      }
+  } while (after !== befor)
+  if(after) return false;
+  else return true;
 }
